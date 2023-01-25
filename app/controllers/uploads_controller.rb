e792a0880d5be928eab3420e576f2f8ef3d98287
harold.camacho <harold.camacho@trueforce.com>
2023-01-25 09:43:38 -0400

    feature-apiCreation

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,29 @@
+class UploadsController < ApplicationController
+    attr_accessor :collection, :divisor
+
+    def create
+        file = params[:file]
+        json_data = JSON.parse(file.read)
+        puts json_data
+        @collection = json_data["number_collection"]
+        @divisor = json_data["div_by"]
+        updated_collection = processCollection(@collection, @divisor)
+        render json: { status: 'success', updated_collection: updated_collection }, status: :ok
+    end
+
+    def dropNumbersThatAreDividedByDivisor (collection, divisor)
+        updated_collection = collection.reject { |n|  n % divisor == 0 }
+        return updated_collection
+    end
+
+    def sortCollection (collection)
+        updated_collection = collection.sort
+        return updated_collection
+    end
+
+    def processCollection (collection, divisor)
+        updated_collection = dropNumbersThatAreDividedByDivisor(collection, divisor)
+        updated_collection = sortCollection(updated_collection)
+        return updated_collection
+    end
+end
---
+++ app/helpers/numbers_processor_helper.rb
@@ -0,0 +1,2 @@
+module NumbersProcessorHelper
+end
